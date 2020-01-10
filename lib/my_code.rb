2101af76fac0c75f7613a85685f0edd71d1fa384
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-10 03:43:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,6 @@ def map_to_negativize(source_array)
     final_array=[]
     i =0
   while i<source_array.length do
-    
     final_array.push(source_array[i]*-1)
     i+=1
   end
@@ -10,6 +9,13 @@ def map_to_negativize(source_array)
 end
 
 def map_to_no_change(source_array)
+   final_array=[]
+    i =0
+  while i<source_array.length do
+    final_array.push(source_array[i])
+    i+=1
+  end
+  final_array
 end
 
 def map_to_double(source_array)

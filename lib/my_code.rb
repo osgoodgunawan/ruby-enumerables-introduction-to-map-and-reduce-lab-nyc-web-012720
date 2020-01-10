d9a1ccf1df97eb26e5bd3fc9c20c71047ee524e4
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-10 03:44:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,6 +19,13 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
+   final_array=[]
+    i =0
+  while i<source_array.length do
+    final_array.push(source_array[i]*2)
+    i+=1
+  end
+  final_array
 end
 
 def map_to_square(source_array)

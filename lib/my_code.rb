5aac7c788874947ce0b2fad8aafac06ffd992571
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-10 03:57:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,7 +43,7 @@ def reduce_to_total(source_array, starting_point)
   total=0
   counter=0
   while counter<source_array.size do 
-    total +=source_array[counter]
+    total =source_array[counter]+total
     counter+=1
   end
   total+starting_point
